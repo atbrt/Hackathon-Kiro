1a57c046ead2838918f7b20714429406a06099c0
Teiki Heinis <156649643+teiki-h@users.noreply.github.com>
2024-11-14 18:39:29 +0100

    Update permutation.py
    permutation fini on a paint_exit

--- permutation.py
+++ permutation.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from fichiersjson import *
 def invert_permutation(p):
     """retourne l'inverse de la permutation p
     """
@@ -13,10 +13,36 @@ def invert_permutation(p):
 def tau(t,delta,n):
     tau_arr=np.arange(1,n+1).tolist()
     delta_t=min(delta,n-t)
-    i=tau_arr.pop(t-1)
-    tau_arr.insert(t+delta_t,i)
+    i=tau_arr.pop(t+delta_t-1)
+    tau_arr.insert(t-1,i)
     return tau_arr
 
 def composition(permu1,permu2):
     #renvoi permu1 rond permu 2
     return np.array(permu1)[np.array(permu2)-1]
+
+def indices_twopaint(dico, l):
+    #dico = le data loader par le json
+    #l la permutation
+    ensemble=[]
+    for k in range(len(dico["vehicles"])):
+        if dico["vehicles"][k]["type"]=="two-tone":
+            ensemble.append(l.index(dico["vehicles"][k]["id"]))
+    return np.array(ensemble)+1
+
+def paint_exit(data,sigma):
+    delta=data["parameters"]["two_tone_delta"]
+    n=len(sigma)
+    sigma_exit=invert_permutation(sigma)
+    
+    t_u_list=np.sort(indices_twopaint(data,sigma))
+    u_list= np.array(sigma)[t_u_list-1]
+    u_list=u_list.tolist()
+    
+    while u_list != []:
+        u=u_list.pop(0)
+        t_u=(sigma_exit)[u-1]
+        tau_u=tau(t_u,delta,n)
+        sigma_exit=composition(tau_u,sigma_exit)
+        
+    return invert_permutation(sigma_exit)
