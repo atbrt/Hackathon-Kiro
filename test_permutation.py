ff12903dd03b2d29a2f855c162bc381022e4e3fd
atbrt <aroldtoubert@gmail.com>
2024-11-14 16:52:47 +0100

    new

---
+++ test_permutation.py
@@ -0,0 +1,16 @@
+
+
+
+
+def indices_twopaint(dico, l):
+    ensemble=[]
+    for k in range(len(dico["vehicles"])):
+        if dico["vehicles"][k]["type"]=="two-twone":
+            ensemble.append(l.index(dico["vehicles"][k]["id"]))
+
+
+    return ensemble
+
+
+
+
